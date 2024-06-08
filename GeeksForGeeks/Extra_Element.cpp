e9067bfdf499bb5abafacbaf5c819c033a123a9d
ChethanJS07 <jschethan75@gmail.com>
2024-06-08 09:01:13 +0530

    GFG Problem of the Day

---
+++ GeeksForGeeks/Extra_Element.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int findExtra(int n, int arr1[], int arr2[]) {
+        for(int i=0;i<n-1;i++){
+            if(arr1[i]!=arr2[i]){
+                return i;
+            }
+        }
+        return n-1;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr1[n], arr2[n - 1];
+        for (int i = 0; i < n; i++) {
+            cin >> arr1[i];
+        }
+        for (int i = 0; i < n - 1; i++) {
+            cin >> arr2[i];
+        }
+        Solution obj;
+        cout << obj.findExtra(n, arr1, arr2) << endl;
+    }
+}
