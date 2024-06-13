8a13c5c9abbef696aef318395472a3463f9d1ccf
ChethanJS07 <jschethan75@gmail.com>
2024-06-13 21:43:28 +0530

    CodeForces Practice Problem

---
+++ CodeForces/A_George_and_Accommodation.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+          
+int main()
+{
+  int n;
+  cin>>n;
+  int count=0;
+  while(n--)
+  {
+    int p,q;
+    cin>>p>>q;
+    if(q-p>=2)
+    {
+      count++;
+    }
+  }
+  cout<<count<<endl;
+}
