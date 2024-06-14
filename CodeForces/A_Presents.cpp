7afdeab0640fbe8abd19ea69c8a69c5170024cfe
ChethanJS07 <jschethan75@gmail.com>
2024-06-14 16:47:07 +0530

    CodeForces Practice Problem

---
+++ CodeForces/A_Presents.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+          
+int main()
+{
+  int n;
+  cin>>n;
+  int a[n],b[n];
+  for(int i=0;i<n;i++)
+  {
+    cin>>a[i];
+  }
+  for(int i=0;i<n;i++)
+  {
+    b[a[i]-1]=i+1;
+  }
+  for(int i=0;i<n;i++)
+  {
+    cout<<b[i]<<" ";
+  }
+
+}
