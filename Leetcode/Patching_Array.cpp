f8e17969d9919385a87ae34de9af5aa3bbb95f4f
ChethanJS07 <jschethan75@gmail.com>
2024-06-16 08:32:00 +0530

    LeetCode Potd 16-06-24

---
+++ Leetcode/Patching_Array.cpp
@@ -0,0 +1,23 @@
+class Solution 
+{
+  public:
+    int minPatches(std::vector<int>& nums, int n) 
+    {
+      long long patch = 0;
+      int count = 0;
+      int index = 0;
+      while (patch < n) 
+      {
+        if (index < nums.size() && patch + 1 >= nums[index]) 
+        {
+          patch += nums[index];
+          index++;
+        } else 
+        {
+          patch += (patch + 1);
+          count++;
+        }
+      }
+      return count;
+    }
+};
