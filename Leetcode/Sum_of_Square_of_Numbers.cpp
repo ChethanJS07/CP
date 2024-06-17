ae63f4f956dec01c7c47c69d70ae436f176f2d84
ChethanJS07 <jschethan75@gmail.com>
2024-06-17 19:51:57 +0530

    LeetCode Potd 17-06-24

--- Leetcode/Sum_of_Square_of_Numbers.cpp
+++ Leetcode/Sum_of_Square_of_Numbers.cpp
@@ -3,10 +3,9 @@ class Solution
 public:
     bool judgeSquareSum(int c) 
     {
-        double b;
         for(int a=0;a<int(sqrt(c))+1;a++)
         {
-            b=sqrt(c-a*a);
+            double b=sqrt(c-a*a);
             if(b==int(b))
             {
                 return true;
