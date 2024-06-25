e0d741b354bddd60e64982465a15fa5448e7d9b1
ChethanJS07 <jschethan75@gmail.com>
2024-06-25 20:10:29 +0530

    LeetCode Potd 25-06-24

--- Leetcode/Greater_Sum_Tree.cpp
+++ Leetcode/Greater_Sum_Tree.cpp
@@ -7,10 +7,10 @@ class Solution
     {
       if (root) 
       {
-        traverse(root->right);  // Traverse the right subtree
-        sum += root->val;  // Update the sum
-        root->val = sum;  // Update the current node's value
-        traverse(root->left);  // Traverse the left subtree
+        traverse(root->right);  
+        sum += root->val;  
+        root->val = sum; 
+        traverse(root->left); 
       }
     }
 
