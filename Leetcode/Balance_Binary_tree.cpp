896a375ba4051a563edb3bf65b865e3ebc31682d
ChethanJS07 <jschethan75@gmail.com>
2024-06-26 16:59:09 +0530

    LeetCode Potd 26-06-24

---
+++ Leetcode/Balance_Binary_tree.cpp
@@ -0,0 +1,35 @@
+class Solution 
+{
+  public:
+    TreeNode* balanceBST(TreeNode* root) 
+    {
+      vector<int> sortedElements;
+      inOrderTraversal(root, sortedElements);
+      return buildBalancedBST(sortedElements, 0, sortedElements.size() - 1);
+    }
+
+  private:
+    void inOrderTraversal(TreeNode* node, vector<int>& sortedElements) 
+    {
+      if(!node) 
+      {
+        return;
+      }
+      inOrderTraversal(node->left, sortedElements);
+      sortedElements.push_back(node->val);
+      inOrderTraversal(node->right, sortedElements);
+    }
+    
+    TreeNode* buildBalancedBST(const vector<int>& elements, int start, int end) 
+    {
+      if(start > end) 
+      {
+        return nullptr;
+      }
+      int mid = start + (end - start) / 2;
+      TreeNode* node = new TreeNode(elements[mid]);
+      node->left = buildBalancedBST(elements, start, mid - 1);
+      node->right = buildBalancedBST(elements, mid + 1, end);
+      return node;
+    }
+};
