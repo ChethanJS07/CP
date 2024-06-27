e8b9cfda7314d56857813944367594cf7b313762
ChethanJS07 <jschethan75@gmail.com>
2024-06-27 20:28:30 +0530

    Educational CodeForces Round 167 (Rated for Div.2)

---
+++ CodeForces/A_Catch_the_Coin.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+          
+int main()
+{
+  int n,x,y;
+  cin>>n;
+  for(int i=0;i<n;i++)
+  {
+    cin>>x>>y;
+    if(y<=-2)
+    {
+      cout<<"NO"<<endl;
+    }
+    else
+    {
+      cout<<"YES"<<endl;
+    }
+  }
+}
