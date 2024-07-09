0d23e0d606dd709b3fb3bf42f319fdea0cd03051
ChethanJS07 <jschethan75@gmail.com>
2024-07-09 20:07:50 +0530

    Practice Problem

---
+++ CodeForces/A_YES_or_YES.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+          
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    string s;
+    cin>>s;
+    transform(s.begin(),s.end(),s.begin(),::tolower);
+    if(s=="yes")
+    {
+      cout<<"YES"<<endl;
+    }
+    else
+    {
+      cout<<"NO"<<endl;
+    }
+  }
+}
