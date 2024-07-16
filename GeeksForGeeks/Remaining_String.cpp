10990f7a9fa0ff22aa007ee31cf5f18ebfc9c275
ChethanJS07 <jschethan75@gmail.com>
2024-07-16 10:45:20 +0530

    GFG Potd - 16-06-24

---
+++ GeeksForGeeks/Remaining_String.cpp
@@ -0,0 +1,18 @@
+class Solution 
+{
+  public:
+    string printString(string s, char ch, int count) 
+    {
+      int i=0;
+      for(;i<s.length();i++)
+      {
+        if(s[i]==ch)
+        {
+          count--;
+          if(!count) break;
+        }
+      }
+      if(i<s.length()-1) return s.substr(i+1);
+      return "";
+    }
+};
