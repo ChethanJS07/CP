2875bb47fba7d5e9530016bf1d6a531e02e550f6
ChethanJS07 <jschethan75@gmail.com>
2024-08-03 18:13:16 +0530

    LeetCode Potd 03-08-24

---
+++ Leetcode/MakeArraysEqual.cpp
@@ -0,0 +1,14 @@
+class Solution 
+{
+  public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) 
+    {
+      sort(arr.begin(), arr.end());
+      sort(target.begin(), target.end());
+      for (int i = 0; i < arr.size(); i++)
+      {
+        if (arr[i] != target[i]) return false;
+      }
+      return true;
+    }
+};
